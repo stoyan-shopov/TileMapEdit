3d7293027cbcf338ea458e86b384a0330592d667
stoyan shopov <stoyan.shopov@gmail.com>
2017-08-08 13:40:08 +0300

    add tile image loading

--- mapeditor.cxx
+++ mapeditor.cxx
@@ -1,3 +1,6 @@
+#include <QFileDialog>
+#include <QMessageBox>
+
 #include "mapeditor.hxx"
 #include "ui_mapeditor.h"
 
@@ -6,9 +9,22 @@ MapEditor::MapEditor(QWidget *parent) :
 	ui(new Ui::MapEditor)
 {
 	ui->setupUi(this);
+	ui->dockWidgetTileSheetContents->layout()->addWidget(tileSheet = new TileSheet);
 }
 
 MapEditor::~MapEditor()
 {
 	delete ui;
+}
+
+void MapEditor::on_pushButtonOpenImage_clicked()
+{
+	auto s = QFileDialog::getOpenFileName(0, "select image to open");
+	QImage image(s);
+	if (image.isNull())
+	{
+		QMessageBox::warning(0, "error opening image", "Error opening image!");
+		return;
+	}
+	tileSheet->setImage(image);
 }
--- mapeditor.hxx
+++ mapeditor.hxx
@@ -2,6 +2,22 @@
 #define MAPEDITOR_HXX
 
 #include <QMainWindow>
+#include <QPainter>
+
+class TileSheet : public QWidget
+{
+	Q_OBJECT
+private:
+	QImage image;
+protected:
+	virtual void paintEvent(QPaintEvent * event)
+	{
+		QPainter p(this);
+		p.drawImage(0, 0, image);
+	}
+public:
+	void setImage(const QImage & image) { this->image = image; }
+};
 
 namespace Ui {
 class MapEditor;
@@ -15,8 +31,12 @@ public:
 	explicit MapEditor(QWidget *parent = 0);
 	~MapEditor();
 	
+private slots:
+	void on_pushButtonOpenImage_clicked();
+	
 private:
 	Ui::MapEditor *ui;
+	TileSheet * tileSheet;
 };
 
 #endif // MAPEDITOR_HXX
